12bdd2a37d32705e74cde250460be0ffeee17b97
diogoatlas206 <diogoatlas206@gmail.com>
2026-08-15 17:28:56 -0300

    Corrige estrutura do projeto e remove pasta duplicada

--- ponto-saas/backend/src/config/db.js
+++
@@ -1,8 +0,0 @@
-const knex = require('knex');
-const knexConfig = require('../../knexfile');
-
-const environment = process.env.NODE_ENV || 'development';
-
-const db = knex(knexConfig[environment]);
-
-module.exports = db;
